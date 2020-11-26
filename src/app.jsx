0f6267350974ef07d159cf80949838f9f78d7ef0
kimyang-sun <berman532@gmail.com>
2020-11-26 19:30:55 +0900

    검색기능 리팩토링 KEY 보안 설정

--- src/app.jsx
+++ src/app.jsx
@@ -3,37 +3,18 @@ import React, { useEffect, useState } from "react";
 import SearchHeader from "./components/serarch_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-const App = () => {
+const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=24&key=AIzaSyBWHiRMxAh8vgSukgZejqL9xMofElkIyLY",
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
+    youtube
+      .mostPopular()
+      .then(items => setVideos(items))
       .catch(error => console.log("error", error));
   }, []);
 
   const onSearch = query => {
-    let requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${query}&type=video&key=AIzaSyBWHiRMxAh8vgSukgZejqL9xMofElkIyLY`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result =>
-        result.items.map(item => ({ ...item, id: item.id.videoId }))
-      )
+    youtube
+      .search(query)
       .then(items => setVideos(items))
       .catch(error => console.log("error", error));
   };
